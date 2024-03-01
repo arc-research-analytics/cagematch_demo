62fb266552e6ed0385441b6112af02d1c1bb46d5
arc-research-analytics <wwright@atlantaregional.org>
2024-03-01 11:54:12 -0500

    updated files

--- Plotly Dash/plotly_dash.py
+++ Plotly Dash/plotly_dash.py
@@ -5,106 +5,115 @@ import geopandas as gpd
 # Building the app
 from dash import Dash, dcc, html
 
-# color ramp to be used by choropleth
-custom_colors_hex = [
-    '#eff3ff',  # lightest shade
-    '#bdd7e7',
-    '#6baed6',
-    '#3182bd',
-    '#08519c'  # darkest shade
-]
+# ------ STEP 1: BUILD THE MAP----------
 
-# create dictionary from above list
-color_discrete_map = {color_hex: color_hex for color_hex in custom_colors_hex}
+# # color ramp to be used by choropleth
+# custom_colors_hex = [
+#     '#eff3ff',  # lightest shade
+#     '#bdd7e7',
+#     '#6baed6',
+#     '#3182bd',
+#     '#08519c'  # darkest shade
+# ]
 
-# read local geoJSON file
-gdf = gpd.read_file('../Data/Fulton_county.geojson')
+# # create dictionary from above list
+# color_discrete_map = {color_hex: color_hex for color_hex in custom_colors_hex}
 
-# Calculate quantiles for population density
-gdf["density_hex"] = pd.qcut(
-    gdf["rPopDensity_e21"],
-    q=len(custom_colors_hex),
-    labels=custom_colors_hex
-)
+# # read local geoJSON file
+# gdf = gpd.read_file('../Data/Fulton_county.geojson')
 
-# instantiate Plotly choropleth map 'figure'
-fig = px.choropleth_mapbox(
-    gdf,
-    geojson=gdf.geometry,
-    locations=gdf.index,
-    color='density_hex',
-    color_discrete_map=color_discrete_map,
-    hover_name="GEOID",
-    hover_data={"GEOID": True, "rPopDensity_e21": True},
-    opacity=0.8,
-    zoom=8.8,  # higher number = more zoomed in
-    center={
-        "lat": 33.85,  # same lat/long value the Streamlit app is using
-        "lon": -84.42
-    },
+# # Calculate quantiles for population density
+# gdf["density_hex"] = pd.qcut(
+#     gdf["rPopDensity_e21"],
+#     q=len(custom_colors_hex),
+#     labels=custom_colors_hex
+# )
 
-    mapbox_style="carto-darkmatter",
-    # mapbox_style="open-street-map",
-    # mapbox_style="carto-positron",
-)
+# # instantiate Plotly choropleth map 'figure'
+# fig = px.choropleth_mapbox(
+#     gdf,
+#     geojson=gdf.geometry,
+#     locations=gdf.index,
+#     color='density_hex',
+#     color_discrete_map=color_discrete_map,
+#     hover_name="GEOID",
+#     hover_data={"GEOID": True, "rPopDensity_e21": True},
+#     opacity=0.8,
+#     zoom=8.8,  # higher number = more zoomed in
+#     center={
+#         "lat": 33.85,  # same lat/long value the Streamlit app is using
+#         "lon": -84.42
+#     },
 
-
-# update map layout options
-fig.update_layout(
-    margin={
-        "r": 0,
-        "t": 30,
-        "l": 0,
-        "b": 0
-    },
-    height=620,
-)
-
-fig.update_traces(
-    showlegend=False,
-    marker_line_width=0.5,
-    marker_line_color='rgb(128,128,128)',
-    hovertemplate="Population / square mile: <b>%{customdata[1]:,.0f}</b><br><br>"
-    "Census Tract %{customdata[0]}<extra></extra>",
-    hoverlabel=dict(
-        bgcolor="rgba(128,128,128,0.75)",  # Background color of the tooltip
-        bordercolor="white",
-        font=dict(family="Arial", size=15, color="white"),
-        align='auto'
-    )
-
-)
-
-# Modebar options
-config = {
-    # 'displayModeBar': False,
-    'displaylogo': False,
-    # 'modeBarButtonsToRemove': [
-    #     'lasso2d',
-    #     'toImage',
-    #     'resetViewMapbox'
-    # ]
-}
+#     mapbox_style="carto-darkmatter",
+#     # mapbox_style="open-street-map",
+#     # mapbox_style="carto-positron",
+# )
 
 
-# create dash application, pass in above 'fig'
+# # update map layout options
+# fig.update_layout(
+#     margin={
+#         "r": 0,
+#         "t": 30,
+#         "l": 0,
+#         "b": 0
+#     },
+#     height=620,
+# )
+
+# # using the 'fig' object created above, we'll modify a few of the components here
+# fig.update_traces(
+#     showlegend=False,
+#     marker_line_width=0.5,
+#     marker_line_color='rgb(128,128,128)',
+#     hovertemplate="Population / square mile: <b>%{customdata[1]:,.0f}</b><br><br>"
+#     "Census Tract %{customdata[0]}<extra></extra>",
+#     hoverlabel=dict(
+#         bgcolor="rgba(128,128,128,0.75)",  # Background color of the tooltip
+#         bordercolor="white",
+#         font=dict(family="Arial", size=15, color="white"),
+#         align='auto'
+#     )
+
+# )
+
+# # Modebar options
+# config = {
+#     # 'displayModeBar': False,
+#     'displaylogo': False,
+#     # 'modeBarButtonsToRemove': [
+#     #     'lasso2d',
+#     #     'toImage',
+#     #     'resetViewMapbox'
+#     # ]
+# }
+
+
+# ------ STEP 2: BUILD THE APP----------
+
+# instantiate the Dash app
 app = Dash()
+
+# page title (aka browser tab title)
 app.title = "Dash Demo"
+
+# define the app's layout usings quasi-HTML elements
 app.layout = html.Div([
-    html.H1(
-        "Fulton County Population Density",
-        style={
-            'textAlign': 'left',
-            'font-size': '2em',
-            'font-family': 'sans-serif',
-            'marginTop': '60px'
-        }),
-    dcc.Graph(
-        figure=fig,
-        config=config,
-        style={
-            'marginTop': '-5px',
-        })
+    # html.H1(
+    #     "Fulton County Population Density",
+    #     style={
+    #         'textAlign': 'left',
+    #         'font-size': '2em',
+    #         'font-family': 'sans-serif',
+    #         'marginTop': '60px'
+    #     }),
+    # dcc.Graph(  # stands for "dash core components"
+    #     figure=fig,
+    #     config=config,
+    #     style={
+    #         'marginTop': '-5px',
+    #     })
 ], style={
     'margin': '50px',
     'padding': '10px'
@@ -112,4 +121,5 @@ app.layout = html.Div([
 
 app.run_server(
     debug=True,
-    dev_tools_ui=False)
+    dev_tools_ui=False
+)
--- Streamlit/streamlit.py
+++ Streamlit/streamlit.py
@@ -4,99 +4,116 @@ import geopandas as gpd
 import pandas as pd
 import pydeck as pdk
 
-# map variables
-latitude = 33.85
-longitude = -84.42
-min_zoom = 8
-max_zoom = 15
-zoom = 8.8  # lower values zoom out, higher values zoom in
-map_height = 590
+# # set page configurations
+# st.set_page_config(
+#     page_title="Streamlit Demo",
+#     page_icon=":sunglasses:",
+#     layout="wide",
+#     initial_sidebar_state="collapsed"
+# )
 
-# set color ramp (using Color Brewer)
-custom_colors_hex = [
-    '#eff3ff',  # lightest shade
-    '#bdd7e7',
-    '#6baed6',
-    '#3182bd',
-    '#08519c'  # darkest shade
-]
+# # Add a map heading - the easy way
+# st.header('Fulton County Population Density')
 
-# convert the above hex list to RGB values
-custom_colors_rgb = [tuple(int(h.lstrip('#')[i:i+2], 16)
-                           for i in (0, 2, 4)) for h in custom_colors_hex]
+# # Add a map heading - the hard way
+# color = '#000000'
+# font_size = '20'
+# font_weight = '200'
+# line_height = '3'
+# st.markdown(
+#     f"<h2 style='color:{color};font-size:{font_size}; font-weight: {font_weight}; line-height: {line_height};'>Fulton County Population Density</h2>",
+#     unsafe_allow_html=True)
 
-# set page configurations
-st.set_page_config(
-    page_title="Streamlit Demo",
-    page_icon=":sunglasses:",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+# # map variables
+# latitude = 33.85
+# longitude = -84.42
+# min_zoom = 8
+# max_zoom = 15
+# zoom = 8.8  # lower values zoom out, higher values zoom in
+# map_height = 590
 
+# # set color ramp (using Color Brewer)
+# custom_colors_hex = [
+#     '#eff3ff',  # lightest shade
+#     '#bdd7e7',
+#     '#6baed6',
+#     '#3182bd',
+#     '#08519c'  # darkest shade
+# ]
 
-def streamlit_mapper():
+# # Pydeck requires RGB values, so we'll convert the above hex list to RGB values
+# custom_colors_rgb = [tuple(int(h.lstrip('#')[i:i+2], 16)
+#                            for i in (0, 2, 4)) for h in custom_colors_hex]
 
-    gdf = gpd.read_file('../Data/Fulton_county.geojson')
+# # read in the data using GeoPandas
+# gdf = gpd.read_file('../Data/Fulton_county.geojson')
 
-    # Create a color mapping using quantiles
-    color_mapper = pd.qcut(gdf['rPopDensity_e21'], q=len(
-        custom_colors_rgb), labels=custom_colors_rgb)
+# # inspect the initial data inside the browser
+# st.write(gdf.columns)
+# gdf_initial = gdf.drop(columns=['geometry'])
+# st.dataframe(gdf_initial)
 
-    # Add the color column
-    gdf["choro_color"] = color_mapper
+# # Create a color map using quantiles
+# color_mapper = pd.qcut(gdf['rPopDensity_e21'], q=len(
+#     custom_colors_rgb), labels=custom_colors_rgb)
 
-    # Format density column
-    gdf['density_formatted'] = gdf['rPopDensity_e21'].apply(
-        lambda x: '{:,.0f}'.format(round(x)))
+# # Add the color column
+# gdf["choro_color"] = color_mapper
 
-    # create map intitial state
-    initial_view_state = pdk.ViewState(
-        latitude=latitude,
-        longitude=longitude,
-        zoom=zoom,
-        max_zoom=max_zoom,
-        min_zoom=min_zoom,
-        height=map_height
-    )
+# # Format density column
+# gdf['density_formatted'] = gdf['rPopDensity_e21'].apply(
+#     lambda x: '{:,.0f}'.format(round(x)))
 
-    # create the geojson layer which will be rendered
-    geojson = pdk.Layer(
-        "GeoJsonLayer",
-        gdf,
-        pickable=True,
-        autoHighlight=True,
-        highlight_color=[255, 255, 255, 128],
-        opacity=0.5,
-        stroked=True,
-        get_line_color=[128, 128, 128],
-        line_width_min_pixels=0.5,
-        filled=True,
-        get_fill_color='choro_color'
-    )
+# # after adding the above columns, let's once again take a look at the data
+# gdf_final = gdf.drop(columns=['geometry'])
+# st.dataframe(gdf_final)
 
-    # configure & customize the tooltip
-    tooltip = {
-        "html": "Population / square mile: <b>{density_formatted}</b><hr style='margin: 10px auto; opacity:1; border-top: 2px solid white; width:85%'>\
-                    Census Tract {GEOID}",
-        "style": {"background": "rgba(128,128,128,0.75)",
-                  "border": "1px solid white",
-                  "color": "white",
-                  "font-family": "Helvetica",
-                  "text-align": "center"
-                  },
-    }
+# # Now let's make a map! The below will configure how the map looks on load
+# initial_view_state = pdk.ViewState(
+#     latitude=latitude,
+#     longitude=longitude,
+#     zoom=zoom,
+#     max_zoom=max_zoom,
+#     min_zoom=min_zoom,
+#     height=map_height
+# )
 
-    # instantiate the map object to be rendered to the Streamlit dashboard
-    r = pdk.Deck(
-        layers=geojson,
-        initial_view_state=initial_view_state,
-        map_provider='carto',  # can also try 'google_maps
-        map_style='dark',  # or light or satellite
-        tooltip=tooltip
-    )
+# # create the geojson layer which will be rendered
+# geojson = pdk.Layer(
+#     "GeoJsonLayer",
+#     gdf,
+#     pickable=True,
+#     autoHighlight=True,
+#     highlight_color=[255, 255, 255, 128],
+#     opacity=0.5,
+#     stroked=True,
+#     get_line_color=[128, 128, 128],
+#     line_width_min_pixels=0.5,
+#     filled=True,
+#     get_fill_color='choro_color'
+# )
 
-    return r
+# # configure & customize the tooltip
+# tooltip = {
+#     "html": "Population / square mile: <b>{density_formatted}</b><hr style='margin: 10px auto; opacity:1; border-top: 2px solid white; width:85%'>\
+#                 Census Tract {GEOID}",
+#     "style": {"background": "rgba(128,128,128,0.75)",
+#               "border": "1px solid white",
+#               "color": "white",
+#               "font-family": "Helvetica",
+#               "text-align": "center"
+#               },
+# }
 
+# # instantiate the map object to be rendered to the browser
+# choropleth_map = pdk.Deck(
+#     layers=geojson,
+#     initial_view_state=initial_view_state,
+#     map_provider='carto',  # can also try 'google_maps
+#     map_style='dark',  # or light or satellite
+#     tooltip=tooltip
+# )
 
-st.header('Fulton County Population Density')
-st.pydeck_chart(streamlit_mapper(), use_container_width=True)
+# # render the map
+# col1, col2, col3 = st.columns(3)
+# st.pydeck_chart(choropleth_map, use_container_width=True)
